21c34ff5347d9badb1b7421328d0cc0a82f021da
Erik <erik.monroy.an@gmail.com>
2023-11-08 07:24:12 -0600

    Animated container terminado

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,15 +1,67 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-
-
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
 
   static const name  = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 59;
+  double height =50;
+  Color color = Colors.indigo;
+  double borderRadius = 20;
+
+  void changeShape(){
+    final random = Random();
+
+    width = random.nextInt(300)  + 50;
+    height = random.nextInt(300)  + 50;
+    borderRadius = random.nextInt(100) + 20;
+
+    color = Color.fromRGBO(
+      random.nextInt(255),// red
+      random.nextInt(255),// green
+      random.nextInt(255), //blue
+      1 //opacity
+    );
+
+    setState(() {
+      
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+
+      body: Center(
+        // Nos va a ayudar para la animacion, tenemos que especificar el tiempo de duracion
+        child: AnimatedContainer(
+          duration:  const Duration(milliseconds: 400),
+          curve: Curves.easeInExpo, // Tipo de animacion que queremos que tenga
+          width: width <= 0 ? 0 :width,
+          height: height <= 0 ? 0 :height,
+          decoration:  BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius <= 0  ? 0 : borderRadius)
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed:  changeShape,
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
